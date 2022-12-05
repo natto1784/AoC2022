eb9ae79611df388da5f9d11ee5a9fb9aeb7eaec4
Amneesh Singh <natto@weirdnatto.in>
2022-12-05 20:16:28 +0530

    day3: update - avoid using unecessary helpers
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- day3.hs
+++ day3.hs
@@ -1,27 +1,24 @@
 import Data.Char (isAsciiLower, isAsciiUpper)
 import Data.List (intersect)
-import Lib (chunks)
 
 main :: IO ()
 main = do
-  input <- readFile "day3.in"
+  input <- lines <$> readFile "day3.in"
   putStr "Q1: "
   print $ q1 input
   putStr "Q2: "
   print $ q2 input
 
-q1 :: String -> Int
+q1 :: [String] -> Int
 q1 =
   sum
     . map (\x -> val . head . uncurry intersect $ splitAt (div (length x) 2) x)
-    . lines
 
-q2 :: String -> Int
-q2 =
-  sum
-    . map (val . head . foldr1 intersect)
-    . chunks 3
-    . lines
+q2 :: [String] -> Int
+q2 [] = 0
+q2 input =
+  let (cur, rest) = splitAt 3 input
+   in (val . head . foldr1 intersect) cur + q2 rest
 
 val :: Char -> Int
 val c
