257e929dd2c2e8a08ed3094aaa8db76f9a89863a
Amneesh Singh <natto@weirdnatto.in>
2022-12-09 18:56:29 +0530

    day 6: rewrite
    use Data.Text
    
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- day6.hs
+++ day6.hs
@@ -1,16 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.List (nub)
+import Lib (readFile')
+import Data.Text (Text)
+import qualified Data.Text as T
 
 main :: IO ()
 main = do
-  input <- readFile "day6.in"
+  input <- readFile' "day6.in"
   putStr "Q1: "
   print $ parse 4 input
   putStr "Q2: "
   print $ parse 14 input
 
-group :: Int -> [Char] -> [[Char]]
-group _ [] = []
-group n xs = take n xs : group n (tail xs)
+group :: Int -> Text -> [Text]
+group _ "" = []
+group n xs = T.take n xs : group n (T.tail xs)
 
-parse :: Int -> String -> Int
-parse n = (+ n) . length . takeWhile ((< n) . length) . map nub . group n
+parse :: Int -> Text -> Int
+parse n = (+ n) . length . takeWhile ((< n) . length) . map (nub . T.unpack) . group n
